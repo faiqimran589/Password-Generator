7370e0592e136b471ca31ecf60576ca8b234f286
faiqimran589 <faiqimran589@gmail.com>
2026-07-18 22:26:40 +0500

    Add user function

--- Password.py
+++ Password.py
@@ -1,12 +1,14 @@
 import random
 import string
+l=int(input('Length of your password is:'))
 characters=(
     string.ascii_letters+
     string.digits+
     string.punctuation
 )
+
 password=""
-for i in range(8):
+for i in range(l):
     password=password+random.choice(characters)
 
 print("your password is:",password)
